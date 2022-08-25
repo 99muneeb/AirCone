0f153323b2d6cef2e91b90ca48a10603dbd943c8
99muneeb <muneeb9166@gmail.com>
2022-08-25 21:20:01 +0500

    static file

--- aircon/settings.py
+++ aircon/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'aircon/static'),
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
