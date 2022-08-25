4928de405f179f2a7d8e0c02b988330ffb90648d
99muneeb <muneeb9166@gmail.com>
2022-08-25 21:31:27 +0500

    static file

--- aircon/urls.py
+++ aircon/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     # path('Appointment/',include('appointment.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-    urlpatterns += static(document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
