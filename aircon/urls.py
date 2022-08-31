d411c2146ad7eda35cf676e2e7f8e5fd643bf7ae
Muneeb Rasheed <59506227+99muneeb@users.noreply.github.com>
2022-08-31 23:13:52 +0500

    first

--- aircon/urls.py
+++ aircon/urls.py
@@ -14,17 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include,re_path
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    # path('Appointment/',include('appointment.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-if settings:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
