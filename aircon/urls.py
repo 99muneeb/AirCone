3a3ee7eeb467f1401ec9d7928497c00486760cd2
Muneeb Rasheed <59506227+99muneeb@users.noreply.github.com>
2022-09-03 00:10:17 +0500

    first

--- aircon/urls.py
+++ aircon/urls.py
@@ -23,7 +23,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
